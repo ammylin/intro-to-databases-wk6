36e07630cbefc6c87be7b50e52b820f681756d5e
Ammy Lin <ammylinnie@gmail.com>
2025-10-04 15:22:32 -0400

    edited rankings.sql

--- scripts/rankings.sql
+++ scripts/rankings.sql
@@ -7,7 +7,7 @@ INSERT INTO university_rankings (institution, country, world_rank, score, year)
 VALUES ('Duke Tech', 'USA', 350, 60.5, 2014);
 
 
---Figure out how many universities from Japan show up in the global top 200 in 2013 (total: 6)
+--Figure out how many universities from Japan show up in the global top 200 in 2013
 SELECT institution
 FROM university_rankings
 WHERE country = 'Japan' AND world_rank <= 200 AND year = 2013;
@@ -20,7 +20,3 @@ WHERE institution = 'University of Oxford' AND year = 2014;
 --Delete all scores below 45 for the year 2015
 DELETE FROM university_rankings
 WHERE score < 45 AND year = 2015;
-
---Show final state of the university_rankings table
-SELECT *
-FROM university_rankings;
